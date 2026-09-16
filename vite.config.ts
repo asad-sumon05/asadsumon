78b67bd398766b03a72448a7e197750ecf099e54
Asaduzzaman Sumon <asad.sumon05@gmail.com>
2026-09-16 20:58:57 +0600

    Fix asset paths for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
-  base: "/asadsumon/",
+  base: "/",
   assetsInclude: ["**/*.glb"],
   server: {
     host: "::",
